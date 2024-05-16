66c95dd53612430dd2cdb40b5cfb0f1cdf488b88
rafaelfmarques <rafaelmarquesclps9@gmail.com>
2024-05-16 10:32:13 -0300

    chore: update prisma schema

---
+++ server/prisma/migrations/20240515224808_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `categories` on the `BookFavorited` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "BookFavorited" DROP COLUMN "categories";
---
+++ server/prisma/migrations/20240515225137_remove_subtitle_prop/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `subtitle` on the `BookFavorited` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "BookFavorited" DROP COLUMN "subtitle";
